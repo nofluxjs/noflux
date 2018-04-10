059fbc8afc6334dce0e812122251bc8305825e95
Malash <i@malash.me>
2018-04-10 17:55:31 +0800

    upgrade react@16.3 and use StrictMode

--- examples/todomvc-react/src/index.js
+++ examples/todomvc-react/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import 'todomvc-app-css/index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<StrictMode><App /></StrictMode>, document.getElementById('root'));
 registerServiceWorker();
